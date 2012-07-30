3e74974ecf7be19680f6c8b2e6a62804defd675f
Filip Hanik <fhanik@vmware.com>
2012-07-30 08:52:09 -0600

    added a simple test case that works against a default rabbitmq server

---
+++ com.rabbitmq.jms/src/test/java/com/rabbitmq/test/java/TestSimpleServerConnection.java
@@ -0,0 +1,119 @@
+package com.rabbitmq.test.java;
+
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+public class TestSimpleServerConnection {
+
+    private volatile String username = "guest";
+    private volatile String password = "guest";
+    private volatile String virtualHost = "/";
+    private volatile String host = "localhost";
+    private volatile int port = 5672;
+    private volatile Connection conn = null;
+
+    @Before
+    public void setUp() throws Exception {
+        AMQP.BasicProperties.Builder bob = new AMQP.BasicProperties.Builder();
+        AMQP.BasicProperties minBasic = bob.build();
+        AMQP.BasicProperties minPersistentBasic = bob.deliveryMode(2).build();
+        AMQP.BasicProperties persistentBasic = bob.priority(0).contentType("application/octet-stream").build();
+        AMQP.BasicProperties persistentTextPlain = bob.contentType("text/plain").build();
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername(getUsername());
+        factory.setPassword(getPassword());
+        factory.setVirtualHost(getVirtualHost());
+        factory.setHost(getHost());
+        factory.setPort(getPort());
+        conn = factory.newConnection();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Connection conn = getConnection();
+        this.conn = null;
+        if (conn != null) {
+            conn.close();
+        }
+    }
+
+    @Test
+    public void test() throws Exception {
+        final Channel channel = getConnection().createChannel();
+        channel.exchangeDeclare("exchangeName", "direct", true);
+        String queueName = channel.queueDeclare().getQueue();
+        channel.queueBind(queueName, "exchangeName", "routingKey");
+        byte[] messageBodyBytes = "Hello, world!".getBytes();
+        channel.basicPublish("exchangeName", "routingKey", null, messageBodyBytes);
+        boolean autoAck = false;
+        channel.basicConsume(queueName, autoAck, "myConsumerTag", new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+                    throws IOException {
+                String routingKey = envelope.getRoutingKey();
+                String contentType = properties.getContentType();
+                long deliveryTag = envelope.getDeliveryTag();
+                // (process the message components here ...)
+                System.out.println("Received Message:" + new String(body));
+                channel.basicAck(deliveryTag, false);
+            }
+        });
+        Thread.sleep(1000);
+        channel.close();
+    }
+
+    public Connection getConnection() {
+        return conn;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getVirtualHost() {
+        return virtualHost;
+    }
+
+    public void setVirtualHost(String virtualHost) {
+        this.virtualHost = virtualHost;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+}
