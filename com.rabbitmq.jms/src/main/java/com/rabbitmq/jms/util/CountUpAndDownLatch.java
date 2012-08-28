d679077520b8ca0a13514df668db20bfb0023151
Filip Hanik <fhanik@vmware.com>
2012-08-28 17:03:27 -0600

    add unit test for counter latch

--- com.rabbitmq.jms/src/main/java/com/rabbitmq/jms/util/CountUpAndDownLatch.java
+++ com.rabbitmq.jms/src/main/java/com/rabbitmq/jms/util/CountUpAndDownLatch.java
@@ -3,6 +3,11 @@ package com.rabbitmq.jms.util;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 
+/**
+ * Similar implementation like the count down latch, except this latch can be counted up 
+ * and counted down. Once it has reached zero, it will allow for a release, but can then be counted up again.
+ * @see {link java.util.concurrent.CountDownLatch}
+ */
 
 public class CountUpAndDownLatch {
 
@@ -33,22 +38,43 @@ public class CountUpAndDownLatch {
 
     private final CountSync countSync;
 
+    /**
+     * Create a latch with the initial value.
+     * @param count
+     */
     public CountUpAndDownLatch(int count) {
         this.countSync = new CountSync(count);
     }
 
+    /**
+     * Blocks and waits until the latch reaches zero or the timeout period has passed
+     * @param timeout - the time to block on the latch
+     * @param unit - the timeunit to be used
+     * @return true if the latch reached zero, false if the timeout period passed
+     * @throws InterruptedException if the thread is interrupted while waiting
+     */
     public boolean awaitZero(long timeout, TimeUnit unit) throws InterruptedException {
         return countSync.tryAcquireSharedNanos(1, unit.toNanos(timeout));
     }
 
+    /**
+     * Counts down the latch, if it reaches zero then it will release the waiting threads
+     */
     public void countDown() {
         countSync.releaseShared(-1);
     }
     
+    /**
+     * Counts up the latch, if it reaches zero then it will release the waiting threads
+     */
     public void countUp() {
         countSync.releaseShared(1);
     }
 
+    /**
+     * Returns the current count value
+     * @return the count value of this latch
+     */
     public long getCount() {
         return countSync.getCount();
     }
