f91a95ded422cae8bc84051f03dcb794b13f3fd6
Steve Powell <steve@rabbitmq.com>
2012-10-12 11:56:59 +0100

    Keep RMQDestination serializable.

--- com.rabbitmq.jms/src/main/java/com/rabbitmq/jms/admin/RMQExchangeInfo.java
+++ com.rabbitmq.jms/src/main/java/com/rabbitmq/jms/admin/RMQExchangeInfo.java
@@ -1,11 +1,21 @@
 package com.rabbitmq.jms.admin;
 
+import java.io.Serializable;
+
 /**
  * RabbitMQ exchange information required by a JMS {@link com.rabbitmq.jms.admin.RMQDestination RMQDestination} object.
  */
-public class RMQExchangeInfo {
+public class RMQExchangeInfo implements Serializable {
     private final String name;
     private final String type;
+
+    /**
+     * Only required for serializability.
+     */
+    public RMQExchangeInfo() {
+        this.name = null;
+        this.type = null;
+    }
 
     public RMQExchangeInfo(String name, String type) {
         this.name = name;
