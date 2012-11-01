998f67ad9d5b1a3bcb267ca9ce54e2ec9bcc766b
Steve Powell <steve@rabbitmq.com>
2012-11-01 11:44:41 +0000

    Eliminate warning re serialization

--- com.rabbitmq.jms/src/main/java/com/rabbitmq/jms/admin/RMQExchangeInfo.java
+++ com.rabbitmq.jms/src/main/java/com/rabbitmq/jms/admin/RMQExchangeInfo.java
@@ -6,6 +6,8 @@ import java.io.Serializable;
  * RabbitMQ exchange information required by a JMS {@link com.rabbitmq.jms.admin.RMQDestination RMQDestination} object.
  */
 public class RMQExchangeInfo implements Serializable {
+    /** TODO */
+    private static final long serialVersionUID = 1L;
     private final String name;
     private final String type;
 
