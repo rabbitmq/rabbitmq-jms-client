2c0ff57d25a6f5abbd5d76d9667872c9b9a6d9cc
Steve Powell <steve@rabbitmq.com>
2012-12-19 11:29:42 +0000

    RJMS-54: Add and annotate assertions in IT test, to improve diagnostics.

--- com.rabbitmq.jms/src/test/java/com/rabbitmq/integration/tests/RabbitMQRedeliverOnNackIT.java
+++ com.rabbitmq.jms/src/test/java/com/rabbitmq/integration/tests/RabbitMQRedeliverOnNackIT.java
@@ -1,5 +1,7 @@
 package com.rabbitmq.integration.tests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import javax.jms.DeliveryMode;
@@ -12,13 +14,11 @@ import javax.jms.QueueSession;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
-import junit.framework.Assert;
-
 import org.junit.Test;
 
 
 /**
- * Integration test
+ * Integration test for re-deliver on rollback.
  */
 public class RabbitMQRedeliverOnNackIT {
     static final String QUEUE_NAME = "test.queue."+RabbitMQRedeliverOnNackIT.class.getCanonicalName();
@@ -26,7 +26,7 @@ public class RabbitMQRedeliverOnNackIT {
 
 
     /**
-     * Test that Rabbit re-delivers a message which has been explicitly NACKed.
+     * Test that Rabbit re-delivers a received message which has been rolled-back.
      * @throws Exception on test failure
      */
     @Test
@@ -37,7 +37,8 @@ public class RabbitMQRedeliverOnNackIT {
         queueConn = connFactory.createQueueConnection();
         try {
             queueConn.start();
-            QueueSession queueSession = queueConn.createQueueSession(false, Session.CLIENT_ACKNOWLEDGE);
+            QueueSession queueSession = queueConn.createQueueSession( false /*not transacted*/
+                                                                    , Session.CLIENT_ACKNOWLEDGE);
             Queue queue = queueSession.createQueue(QUEUE_NAME);
             QueueSender queueSender = queueSession.createSender(queue);
             queueSender.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
@@ -48,20 +49,27 @@ public class RabbitMQRedeliverOnNackIT {
         }
         queueConn = connFactory.createQueueConnection();
         try {
-            queueConn.start();
-            QueueSession queueSession = queueConn.createQueueSession(true, Session.DUPS_OK_ACKNOWLEDGE);
-            Queue queue = queueSession.createQueue(QUEUE_NAME);
-            QueueReceiver queueReceiver = queueSession.createReceiver(queue);
-            TextMessage message = (TextMessage) queueReceiver.receive();
-            Assert.assertEquals(MESSAGE, message.getText());
-            queueSession.rollback();
-            message = (TextMessage) queueReceiver.receive();
-            Assert.assertEquals(MESSAGE, message.getText());
-            assertTrue(message.getJMSRedelivered());
-            queueSession.commit();
+        queueConn.start();
+        QueueSession queueSession = queueConn.createQueueSession( true /*transacted*/
+                                                                , Session.DUPS_OK_ACKNOWLEDGE);
+        Queue queue = queueSession.createQueue(QUEUE_NAME);
+        QueueReceiver queueReceiver = queueSession.createReceiver(queue);
+        TextMessage message = (TextMessage) queueReceiver.receive();
+        assertNotNull("No message delivered initially", message);
+        assertEquals("Wrong message delivered initially", MESSAGE, message.getText());
+
+        queueSession.rollback();
+
+        message = (TextMessage) queueReceiver.receive();
+
+        assertNotNull("No message delivered after rollback", message);
+        assertEquals("Wrong message redelivered", MESSAGE, message.getText());
+        assertTrue("Message not marked 'redelivered'", message.getJMSRedelivered());
+
+        queueSession.commit();
         } finally {
             queueConn.close();
-        }
-
     }
+
+}
 }
