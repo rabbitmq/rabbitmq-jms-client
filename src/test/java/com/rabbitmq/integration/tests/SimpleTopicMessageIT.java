0a88ce82c7cda1f37e546c47226b9146ea8df0e0
Steve Powell <steve@rabbitmq.com>
2013-09-19 13:09:21 +0100

    [v1.0.x]: Add test for wildcard Topic messages (fails).

--- src/test/java/com/rabbitmq/integration/tests/SimpleTopicMessageIT.java
+++ src/test/java/com/rabbitmq/integration/tests/SimpleTopicMessageIT.java
@@ -18,10 +18,12 @@ import org.junit.Test;
  */
 public class SimpleTopicMessageIT extends AbstractITTopic {
     private static final String TOPIC_NAME = "test.topic." + SimpleTopicMessageIT.class.getCanonicalName();
+    private static final String WILD_TOPIC_NAME = "test.topic.#";
+    private static final String MESSAGE_TEXT_1 = "Hello " + SimpleTopicMessageIT.class.getName();
+    private static final String MESSAGE_TEXT_2 = "Wild hello " + SimpleTopicMessageIT.class.getName();
 
     @Test
     public void testSendAndReceiveTextMessage() throws Exception {
-        final String MESSAGE2 = "Hello " + SimpleTopicMessageIT.class.getName();
         topicConn.start();
         TopicSession topicSession = topicConn.createTopicSession(false, Session.DUPS_OK_ACKNOWLEDGE);
         Topic topic = topicSession.createTopic(TOPIC_NAME);
@@ -30,10 +32,29 @@ public class SimpleTopicMessageIT extends AbstractITTopic {
         TopicSubscriber receiver2 = topicSession.createSubscriber(topic);
 
         sender.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-        TextMessage message = topicSession.createTextMessage(MESSAGE2);
+        TextMessage message = topicSession.createTextMessage(MESSAGE_TEXT_1);
         sender.send(message);
 
-        Assert.assertEquals(MESSAGE2, ((TextMessage) receiver1.receive()).getText());
-        Assert.assertEquals(MESSAGE2, ((TextMessage) receiver2.receive()).getText());
+        assertEquals(MESSAGE_TEXT_1, ((TextMessage) receiver1.receive()).getText());
+        assertEquals(MESSAGE_TEXT_1, ((TextMessage) receiver2.receive()).getText());
+    }
+
+    @Test
+    public void testSendAndReceiveWildTopicTestMessage() throws Exception {
+        topicConn.start();
+        TopicSession topicSession = topicConn.createTopicSession(false, Session.DUPS_OK_ACKNOWLEDGE);
+        Topic explicitTopicDestination = topicSession.createTopic(TOPIC_NAME);
+        Topic wildTopicDestination = topicSession.createTopic(WILD_TOPIC_NAME);
+
+        TopicPublisher sender = topicSession.createPublisher(explicitTopicDestination);
+        TopicSubscriber receiver = topicSession.createSubscriber(wildTopicDestination);
+
+        sender.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+        TextMessage message = topicSession.createTextMessage(MESSAGE_TEXT_2);
+        sender.send(message);
+
+        TextMessage textMessage = (TextMessage) receiver.receive();
+        assertEquals("Failed to receive proper text message", MESSAGE_TEXT_2, textMessage.getText());
+        assertEquals("Incorrect publisher destination on received message", explicitTopicDestination, (Topic) textMessage.getJMSDestination());
     }
 }
