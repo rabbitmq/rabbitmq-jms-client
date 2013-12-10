6044bc90643d483606ef6fbc05e30fa8e693b56a
Steve Powell <steve@rabbitmq.com>
2013-12-10 17:08:01 +0000

    [sql-parsing#54038411]: Eliminate sacrificial channel (for topic selector binding failures), and reply upon local type-checking instead.

--- src/main/java/com/rabbitmq/jms/util/RMQJMSSelectorException.java
+++ src/main/java/com/rabbitmq/jms/util/RMQJMSSelectorException.java
@@ -22,4 +22,8 @@ public class RMQJMSSelectorException extends InvalidSelectorException {
         super(msg, errorCode);
         this.initCause(x);
     }
+
+    public RMQJMSSelectorException(String msg) {
+        super(msg);
+    }
 }
