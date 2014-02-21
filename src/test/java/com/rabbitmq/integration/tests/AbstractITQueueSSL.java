638452d6c9b353c76aa2c55777bd9ac7741d43f2
Steve Powell <steve@rabbitmq.com>
2014-02-21 17:41:05 +0000

    Consolidate AbstractITQueueSSL as extension of AbstractITQueue; Simplify SSLSimpleQueueMessageIT as virtual copy of SimpleQueueMessageIT.

--- src/test/java/com/rabbitmq/integration/tests/AbstractITQueueSSL.java
+++ src/test/java/com/rabbitmq/integration/tests/AbstractITQueueSSL.java
@@ -1,37 +1,16 @@
 /* Copyright (c) 2013 Pivotal Software, Inc. All rights reserved. */
 package com.rabbitmq.integration.tests;
 
-import static org.junit.Assert.fail;
-
-import javax.jms.QueueConnection;
 import javax.jms.QueueConnectionFactory;
 
-import org.junit.After;
 import org.junit.Before;
 
-public abstract class AbstractITQueueSSL {
-    protected QueueConnectionFactory connFactory;
-    protected QueueConnection queueConn;
+public abstract class AbstractITQueueSSL extends AbstractITQueue {
 
     @Before
     public void beforeTests() throws Exception {
         this.connFactory = (QueueConnectionFactory) AbstractTestConnectionFactory.getSslTestConnectionFactory()
                 .getConnectionFactory();
         this.queueConn = connFactory.createQueueConnection();
-    }
-
-    protected void reconnect() throws Exception {
-        if (queueConn != null) {
-            this.queueConn.close();
-            this.queueConn = connFactory.createQueueConnection();
-        } else {
-            fail("Cannot reconnect");
-        }
-    }
-
-    @After
-    public void afterTests() throws Exception {
-        if (queueConn != null)
-            queueConn.close();
     }
 }
--- src/test/java/com/rabbitmq/integration/tests/SSLSimpleQueueMessageIT.java
+++ src/test/java/com/rabbitmq/integration/tests/SSLSimpleQueueMessageIT.java
@@ -1,25 +1,16 @@
 /* Copyright (c) 2013 Pivotal Software, Inc. All rights reserved. */
 package com.rabbitmq.integration.tests;
 
-import static org.junit.Assert.assertEquals;
-
 import java.io.Serializable;
 
-import javax.jms.BytesMessage;
 import javax.jms.DeliveryMode;
-import javax.jms.MapMessage;
-import javax.jms.ObjectMessage;
 import javax.jms.Queue;
 import javax.jms.QueueReceiver;
 import javax.jms.QueueSender;
 import javax.jms.QueueSession;
 import javax.jms.Session;
-import javax.jms.StreamMessage;
-import javax.jms.TextMessage;
 
 import org.junit.Test;
-
-import com.rabbitmq.jms.client.message.TestMessages;
 
 /**
  * Integration test for simple point-to-point messaging.
@@ -27,19 +18,19 @@ import com.rabbitmq.jms.client.message.TestMessages;
 public class SSLSimpleQueueMessageIT extends AbstractITQueueSSL {
 
     private static final String QUEUE_NAME = "test.queue."+SSLSimpleQueueMessageIT.class.getCanonicalName();
-    private static final String MESSAGE = "Hello " + SSLSimpleQueueMessageIT.class.getName();
     private static final long TEST_RECEIVE_TIMEOUT = 1000; // one second
 
-    @Test
-    public void testSendAndReceiveTextMessage() throws Exception {
+    private void messageTestBase(MessageTestType mtt) throws Exception {
         try {
             queueConn.start();
             QueueSession queueSession = queueConn.createQueueSession(false, Session.DUPS_OK_ACKNOWLEDGE);
             Queue queue = queueSession.createQueue(QUEUE_NAME);
+
+            drainQueue(queueSession, queue);
+
             QueueSender queueSender = queueSession.createSender(queue);
             queueSender.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-            TextMessage message = queueSession.createTextMessage(MESSAGE);
-            queueSender.send(message);
+            queueSender.send(mtt.gen(queueSession, (Serializable)queue));
         } finally {
             reconnect();
         }
@@ -48,103 +39,36 @@ public class SSLSimpleQueueMessageIT extends AbstractITQueueSSL {
         QueueSession queueSession = queueConn.createQueueSession(false, Session.DUPS_OK_ACKNOWLEDGE);
         Queue queue = queueSession.createQueue(QUEUE_NAME);
         QueueReceiver queueReceiver = queueSession.createReceiver(queue);
-        TextMessage message = (TextMessage) queueReceiver.receive(TEST_RECEIVE_TIMEOUT);
-        assertEquals(MESSAGE, message.getText());
+        mtt.check(queueReceiver.receive(TEST_RECEIVE_TIMEOUT), (Serializable)queue);
     }
 
     @Test
-    public void testSendAndReceiveBytesMessage() throws Exception {
-        try {
-            queueConn.start();
-            QueueSession queueSession = queueConn.createQueueSession(false, Session.DUPS_OK_ACKNOWLEDGE);
-            Queue queue = queueSession.createQueue(QUEUE_NAME);
-            QueueSender queueSender = queueSession.createSender(queue);
-            queueSender.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-            BytesMessage message = queueSession.createBytesMessage();
-
-            TestMessages.writeBytesMessage(message);
-            queueSender.send(message);
-        } finally {
-            reconnect();
-        }
-
-        queueConn.start();
-        QueueSession queueSession = queueConn.createQueueSession(false, Session.DUPS_OK_ACKNOWLEDGE);
-        Queue queue = queueSession.createQueue(QUEUE_NAME);
-        QueueReceiver queueReceiver = queueSession.createReceiver(queue);
-        BytesMessage message = (BytesMessage) queueReceiver.receive(TEST_RECEIVE_TIMEOUT);
-        TestMessages.readBytesMessage(message);
+    public void testSendBrowseAndReceiveLongTextMessage() throws Exception {
+        messageTestBase(MessageTestType.LONG_TEXT);
     }
 
     @Test
-    public void testSendAndReceiveMapMessage() throws Exception {
-        try {
-            queueConn.start();
-            QueueSession queueSession = queueConn.createQueueSession(false, Session.DUPS_OK_ACKNOWLEDGE);
-            Queue queue = queueSession.createQueue(QUEUE_NAME);
-            QueueSender queueSender = queueSession.createSender(queue);
-            queueSender.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-            MapMessage message = queueSession.createMapMessage();
-
-            TestMessages.writeMapMessage(message);
-            queueSender.send(message);
-        } finally {
-            reconnect();
-        }
-
-        queueConn.start();
-        QueueSession queueSession = queueConn.createQueueSession(false, Session.DUPS_OK_ACKNOWLEDGE);
-        Queue queue = queueSession.createQueue(QUEUE_NAME);
-        QueueReceiver queueReceiver = queueSession.createReceiver(queue);
-        MapMessage message = (MapMessage) queueReceiver.receive(TEST_RECEIVE_TIMEOUT);
-        TestMessages.readMapMessage(message);
+    public void testSendBrowseAndReceiveTextMessage() throws Exception {
+        messageTestBase(MessageTestType.TEXT);
     }
 
     @Test
-    public void testSendAndReceiveStreamMessage() throws Exception {
-        try {
-            queueConn.start();
-            QueueSession queueSession = queueConn.createQueueSession(false, Session.DUPS_OK_ACKNOWLEDGE);
-            Queue queue = queueSession.createQueue(QUEUE_NAME);
-            QueueSender queueSender = queueSession.createSender(queue);
-            queueSender.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-            StreamMessage message = queueSession.createStreamMessage();
-            TestMessages.writeStreamMessage(message);
-            queueSender.send(message);
-        } finally {
-            reconnect();
-        }
-
-        queueConn.start();
-        QueueSession queueSession = queueConn.createQueueSession(false, Session.DUPS_OK_ACKNOWLEDGE);
-        Queue queue = queueSession.createQueue(QUEUE_NAME);
-        QueueReceiver queueReceiver = queueSession.createReceiver(queue);
-        StreamMessage message = (StreamMessage) queueReceiver.receive(TEST_RECEIVE_TIMEOUT);
-        TestMessages.readStreamMessage(message);
+    public void testSendBrowseAndReceiveBytesMessage() throws Exception {
+        messageTestBase(MessageTestType.BYTES);
     }
 
     @Test
-    public void testSendAndReceiveObjectMessage() throws Exception {
-        try {
-            queueConn.start();
-            QueueSession queueSession = queueConn.createQueueSession(false, Session.DUPS_OK_ACKNOWLEDGE);
-            Queue queue = queueSession.createQueue(QUEUE_NAME);
-            QueueSender queueSender = queueSession.createSender(queue);
-            queueSender.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-            ObjectMessage message = queueSession.createObjectMessage();
-            message.setObjectProperty("objectProp", "This is an object property"); // try setting an object property, too
-            //TestMessages.writeObjectMessage(message);
-            message.setObject((Serializable)queue);
-            queueSender.send(message);
-        } finally {
-            reconnect();
-        }
+    public void testSendBrowseAndReceiveMapMessage() throws Exception {
+        messageTestBase(MessageTestType.MAP);
+    }
 
-        queueConn.start();
-        QueueSession queueSession = queueConn.createQueueSession(false, Session.DUPS_OK_ACKNOWLEDGE);
-        Queue queue = queueSession.createQueue(QUEUE_NAME);
-        QueueReceiver queueReceiver = queueSession.createReceiver(queue);
-        ObjectMessage message = (ObjectMessage) queueReceiver.receive(TEST_RECEIVE_TIMEOUT);
-        assertEquals("Object not read correctly;", queue.getQueueName(), ((Queue) message.getObject()).getQueueName());
+    @Test
+    public void testSendBrowseAndReceiveStreamMessage() throws Exception {
+        messageTestBase(MessageTestType.STREAM);
+    }
+
+    @Test
+    public void testSendBrowseAndReceiveObjectMessage() throws Exception {
+        messageTestBase(MessageTestType.OBJECT);
     }
 }
