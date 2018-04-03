c8c3687d6d0d9992c6ce945957ba6174333fc93f
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2018-04-03 11:49:14 +0200

    Install broker in stable directory
    [#156274540]

--- bin/before_build.sh
+++ bin/before_build.sh
@@ -7,11 +7,11 @@ make -C /tmp/tls-gen/basic
 
 wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.7.4/rabbitmq-server-generic-unix-3.7.4.tar.xz
 tar xf rabbitmq-server-generic-unix-3.7.4.tar.xz
+mv rabbitmq_server-3.7.4 rabbitmq
 
-cp target/test-classes/rabbit@localhost.config rabbitmq_server-3.7.4/etc/rabbitmq/rabbitmq.config
-rabbitmq_server-3.7.4/sbin/rabbitmq-plugins enable rabbitmq_jms_topic_exchange
-rabbitmq_server-3.7.4/sbin/rabbitmq-server -detached
-export RABBITMQ_CTL=rabbitmq_server-3.7.4/sbin/rabbitmqctl
+cp target/test-classes/rabbit@localhost.config rabbitmq/etc/rabbitmq/rabbitmq.config
+rabbitmq/sbin/rabbitmq-plugins enable rabbitmq_jms_topic_exchange
+rabbitmq/sbin/rabbitmq-server -detached
 
 sleep 3
 
