50099e03f678a5907e7fd39b6f6ef75f961f4c8b
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2018-09-14 15:15:19 +0200

    Add sanity check script

---
+++ sanity-check.groovy
@@ -0,0 +1,34 @@
+@GrabResolver(name = 'rabbitmq-bintray', root = 'http://dl.bintray.com/rabbitmq/maven')
+@GrabResolver(name = 'rabbitmq-bintray-milestones', root = 'http://dl.bintray.com/rabbitmq/maven-milestones')
+@Grab(group = 'com.rabbitmq.jms', module = 'rabbitmq-jms', version = "${version}")
+@Grab(group = 'org.slf4j', module = 'slf4j-simple', version = '1.7.25')
+import com.rabbitmq.jms.admin.RMQConnectionFactory
+import org.slf4j.LoggerFactory
+
+import javax.jms.DeliveryMode
+import javax.jms.QueueReceiver
+import javax.jms.QueueSender
+import javax.jms.QueueSession
+import javax.jms.Session
+
+def connection = new RMQConnectionFactory().createConnection()
+try {
+    connection.start();
+    QueueSession queueSession = connection.createQueueSession(false, Session.DUPS_OK_ACKNOWLEDGE);
+    def queue = queueSession.createTemporaryQueue()
+
+    QueueSender queueSender = queueSession.createSender(queue)
+    queueSender.setDeliveryMode(DeliveryMode.NON_PERSISTENT)
+    def message = queueSession.createTextMessage("Hello")
+    queueSender.send(message)
+
+    QueueReceiver queueReceiver = queueSession.createReceiver(queue)
+    message = queueReceiver.receive(5000)
+    if (message == null)
+        throw new IllegalStateException("Didn't receive message in 5 seconds")
+    LoggerFactory.getLogger("rabbitmq").info("Test succeeded")
+    System.exit 0
+} catch (Exception e) {
+    LoggerFactory.getLogger("rabbitmq").info("Test failed", e)
+    System.exit 1
+}
