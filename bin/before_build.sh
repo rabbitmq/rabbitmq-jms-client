89f2d87826126535a7b7303cda955a04a8f6e254
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2019-08-30 14:23:25 +0200

    Bump Erlang and RabbitMQ on Travis CI

--- bin/before_build.sh
+++ bin/before_build.sh
@@ -5,9 +5,9 @@ git clone https://github.com/michaelklishin/tls-gen.git /tmp/tls-gen
 make -C /tmp/tls-gen/basic
 ./mvnw clean resources:testResources -Dtest-tls-certs.dir=/tmp/tls-gen/basic
 
-wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.7.15/rabbitmq-server-generic-unix-3.7.15.tar.xz
-tar xf rabbitmq-server-generic-unix-3.7.15.tar.xz
-mv rabbitmq_server-3.7.15 rabbitmq
+wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.7.17/rabbitmq-server-generic-unix-3.7.17.tar.xz
+tar xf rabbitmq-server-generic-unix-3.7.17.tar.xz
+mv rabbitmq_server-3.7.17 rabbitmq
 
 cp target/test-classes/rabbit@localhost.config rabbitmq/etc/rabbitmq/rabbitmq.config
 rabbitmq/sbin/rabbitmq-plugins enable rabbitmq_jms_topic_exchange
