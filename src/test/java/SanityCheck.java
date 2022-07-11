e680289d7f45a7b899f6541a16a51be6c52a9c2d
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2022-07-11 11:00:13 +0200

    Add version placeholder to sanity check

--- src/test/java/SanityCheck.java
+++ src/test/java/SanityCheck.java
@@ -1,6 +1,6 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //REPOS mavencentral,ossrh-staging=https://oss.sonatype.org/content/groups/staging/,rabbitmq-packagecloud-milestones=https://packagecloud.io/rabbitmq/maven-milestones/maven2
-//DEPS com.rabbitmq.jms:rabbitmq-jms:2.5.0.RC1
+//DEPS com.rabbitmq.jms:rabbitmq-jms:${version}
 //DEPS org.slf4j:slf4j-simple:1.7.36
 
 import com.rabbitmq.jms.admin.RMQConnectionFactory;
