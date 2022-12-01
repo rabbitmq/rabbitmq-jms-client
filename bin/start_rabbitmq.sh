6926b3981ff99a00f87b8561aadc22a5c66d29ce
Marcial Rosales <mrosales@pivotal.io>
2022-12-01 10:29:55 +0100

    Fix wrong rabbitmq image and tag

--- bin/start_rabbitmq.sh
+++ bin/start_rabbitmq.sh
@@ -2,8 +2,8 @@
 
 LOCAL_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-RABBITMQ_IMAGE_TAG=${RABBITMQ_IMAGE_TAG:-3.11.0-rc.2.29.geee5757.dirty}
-RABBITMQ_IMAGE=${RABBITMQ_IMAGE:-pivotalrabbitmq/rabbitmq}
+RABBITMQ_IMAGE_TAG=${RABBITMQ_IMAGE_TAG:-3.11.3}
+RABBITMQ_IMAGE=${RABBITMQ_IMAGE:-rabbitmq}
 
 wait_for_message() {
   while ! docker logs $1 | grep -q "$2";
