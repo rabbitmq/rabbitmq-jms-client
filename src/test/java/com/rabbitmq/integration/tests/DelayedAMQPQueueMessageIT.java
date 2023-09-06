8290ec2f5918c6ce6c48795d7daaa36ae579ff75
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2023-09-06 09:35:07 +0200

    Disable delayed message tests
    While delayed message exchange plugin is broker on 3.13.

--- src/test/java/com/rabbitmq/integration/tests/DelayedAMQPQueueMessageIT.java
+++ src/test/java/com/rabbitmq/integration/tests/DelayedAMQPQueueMessageIT.java
@@ -12,6 +12,7 @@ import jakarta.jms.JMSContext;
 import jakarta.jms.JMSProducer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -21,6 +22,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
  * Integration test
  */
 @SkipIfDelayedMessageExchangePluginNotActivated
+@Disabled
 public class DelayedAMQPQueueMessageIT extends AbstractAmqpITQueue {
 
     String queueName = "DelayedAMQPQueueMessageIT";
