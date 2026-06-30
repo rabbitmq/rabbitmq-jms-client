09d727d6af70ce24a3652b4743f4f20a72fa82c5
Arnaud Cogoluègnes <514737+acogoluegnes@users.noreply.github.com>
2026-06-30 13:13:02 +0200

    Remove debug log

--- src/main/java/com/rabbitmq/jms/admin/RMQConnectionFactory.java
+++ src/main/java/com/rabbitmq/jms/admin/RMQConnectionFactory.java
@@ -534,7 +534,6 @@ public class RMQConnectionFactory implements ConnectionFactory, Referenceable, S
             factory.useSslProtocol(SSLContext.getDefault());
           } else {
             if (this.sslContext != null) {
-            System.out.println("passe maybe");
               factory.useSslProtocol(this.sslContext);
             } else if (this.tlsProtocol != null) {
               factory.useSslProtocol(this.tlsProtocol);
