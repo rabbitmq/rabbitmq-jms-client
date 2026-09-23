61b58aad3c27b0769270c24a3cddd83d0d665f4b
Arnaud Cogoluègnes <514737+acogoluegnes@users.noreply.github.com>
2026-09-23 14:29:25 +0200

    ci: remove delayed message exchange plugin
    Not maintained anymore.
    
    (cherry picked from commit 10946a770dee8ccecbf81a8a7fa6c3a2e37793f0)

--- ci/start-broker.sh
+++ ci/start-broker.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 RABBITMQ_IMAGE=${RABBITMQ_IMAGE:-rabbitmq:4.3}
-DELAYED_MESSAGE_EXCHANGE_PLUGIN_VERSION=${DELAYED_MESSAGE_EXCHANGE_PLUGIN_VERSION:-4.2.0}
 
 wait_for_message() {
   while ! docker logs "$1" | grep -q "$2";
@@ -20,7 +19,7 @@ cp -R "${PWD}"/tls-gen/basic/result/* rabbitmq-configuration/tls
 chmod o+r rabbitmq-configuration/tls/*
 chmod g+r rabbitmq-configuration/tls/*
 
-echo "[rabbitmq_jms_topic_exchange,rabbitmq_delayed_message_exchange]." > rabbitmq-configuration/enabled_plugins
+echo "[rabbitmq_jms_topic_exchange]." > rabbitmq-configuration/enabled_plugins
 
 echo "loopback_users = none
 
@@ -38,13 +37,10 @@ docker rm -f rabbitmq 2>/dev/null || echo "rabbitmq was not running"
 docker run -d --name rabbitmq \
     -p 5671:5671 -p 5672:5672 \
     -v "${PWD}"/rabbitmq-configuration:/etc/rabbitmq \
-    -v "${PWD}"/ci/rabbitmq_delayed_message_exchange-"${DELAYED_MESSAGE_EXCHANGE_PLUGIN_VERSION}".ez:/opt/rabbitmq/plugins/rabbitmq_delayed_message_exchange-"${DELAYED_MESSAGE_EXCHANGE_PLUGIN_VERSION}".ez \
     "${RABBITMQ_IMAGE}"
 
 wait_for_message rabbitmq "completed with"
 
 docker exec rabbitmq rabbitmqctl enable_feature_flag --opt-in khepri_db
-docker exec rabbitmq rabbitmq-plugins disable rabbitmq_delayed_message_exchange
-docker exec rabbitmq rabbitmq-plugins enable rabbitmq_delayed_message_exchange
 docker exec rabbitmq rabbitmq-diagnostics erlang_version
 docker exec rabbitmq rabbitmqctl version
